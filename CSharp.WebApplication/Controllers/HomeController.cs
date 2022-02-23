67fed3678a102fc626ba92b0511a258607b981d1
Tươi Lê Văn <tuoith95@gmail.com>
2022-02-24 00:15:56 +0700

    commit

--- CSharp.WebApplication/Controllers/HomeController.cs
+++ CSharp.WebApplication/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace CSharp.WebApplication.Controllers
         {
             return View();
         }
+        
+        public IActionResult Demo()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- eShopSolution.Data/EF/EShopDBContext.cs
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -13,7 +13,21 @@ namespace eShopSolution.Data.EF
             
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<AppConfig> AppConfigs { get; set; }
         public DbSet<Product> Products { set; get; }
-        public DbSet<Category> Categories { set; get; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
     }
 }
