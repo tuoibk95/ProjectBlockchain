b1ce62a9f05d1a5e4f8776592b55cd8bcd7eafb5
Tươi Lê Văn <tuoith95@gmail.com>
2022-05-26 02:19:46 +0700

    add API backend

---
+++ eShopSolution.BackendApi/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using eShopSolution.BackendApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
