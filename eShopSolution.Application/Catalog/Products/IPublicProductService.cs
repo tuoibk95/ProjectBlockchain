ea0df1182bd30079aa7de9e252986bbb948c8805
Tươi Lê Văn <tuoith95@gmail.com>
2022-05-26 01:12:39 +0700

    commit

--- eShopSolution.Application/Catalog/Products/IPublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -1,5 +1,4 @@
 ﻿using eShopSolution.ViewModels.Catalog.Products;
-using eShopSolution.ViewModels.Catalog.Products.Public;
 using eShopSolution.ViewModels.Common;
 using System.Threading.Tasks;
 
@@ -7,6 +6,6 @@ namespace eShopSolution.Application.Catalog.Products
 {
     public interface IPublicProductService
     {
-        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetProductPagingRequest request);
+        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request);
     }
 }
